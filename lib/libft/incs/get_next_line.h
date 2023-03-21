fe17dcd117c6f281e6e9ff43037e19a651b3e614
88-yana <85784752+88-yana@users.noreply.github.com>
2023-03-22 03:35:59 +0900

    REPLACE:GNL

--- lib/libft/incs/get_next_line.h
+++ lib/libft/incs/get_next_line.h
@@ -3,40 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmuramat <mt15hydrangea@gmail.com>         +#+  +:+       +#+        */
+/*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/07 16:58:30 by tmuramat          #+#    #+#             */
-/*   Updated: 2022/03/19 11:25:10 by tmuramat         ###   ########.fr       */
+/*   Created: 2022/05/06 20:36:48 by hyanagim          #+#    #+#             */
+/*   Updated: 2023/03/22 03:18:43 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
-# include <unistd.h>
-# include "libft.h"
-
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2042
+#  define BUFFER_SIZE 42
 # endif
 
-# define FD_MAX 256
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-# define RETURN 1
-# define CONTINUE 0
-# define ERROR -1
-# define END -2
+# define FILE_OPEN_MAX 255
 
-typedef struct s_minfo {
-	char	*mem;
-	char	*ptr_nl;
-	int		sts;
-}	t_minfo;
-
+size_t	gt_strlen(const char *str);
+char	*gt_strlcpy(char **save, size_t start, size_t n);
+char	*gt_free(char **save, char **buf);
+int		gt_find_nl(char *s);
 char	*get_next_line(int fd);
-void	read_buffer(int fd, char *buff, t_minfo *input);
-char	*output_one_line(t_minfo *input);
-char	*free_memory(t_minfo *input);
 
 #endif
--- lib/libft/srcs/get_next_line/get_next_line.c
+++ lib/libft/srcs/get_next_line/get_next_line.c
@@ -3,97 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmuramat <mt15hydrangea@gmail.com>         +#+  +:+       +#+        */
+/*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/07 16:49:25 by tmuramat          #+#    #+#             */
-/*   Updated: 2022/03/19 11:32:45 by tmuramat         ###   ########.fr       */
+/*   Created: 2022/05/06 20:36:44 by hyanagim          #+#    #+#             */
+/*   Updated: 2023/03/22 03:21:29 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-char	*free_memory(t_minfo *input)
+static char	*gt_strjoin(char **save, char **buf)
 {
-	free (input->mem);
-	input->mem = NULL;
-	return (NULL);
+	size_t	save_len;
+	size_t	buf_len;
+	char	*line;
+	size_t	i;
+
+	save_len = gt_strlen(*save);
+	buf_len = gt_strlen(*buf);
+	line = malloc(save_len + buf_len + 1);
+	if (line == NULL)
+		return (gt_free(save, buf));
+	i = 0;
+	while (i < save_len)
+	{
+		line[i] = (*save)[i];
+		i++;
+	}
+	while (i < save_len + buf_len + 1)
+	{
+		line[i] = (*buf)[i - save_len];
+		i++;
+	}
+	gt_free(save, NULL);
+	*save = line;
+	return (*save);
 }
 
-char	*output_one_line(t_minfo *input)
+static char	*gt_strclip(char **save, char **buf)
 {
-	char	*ret_line;
-	char	*tmp_memory;
+	size_t	s_len;
+	size_t	i;
+	char	*line;
 
-	if (*input->mem == '\0')
-		return (free_memory(input));
-	else if (input->sts == END)
-		return (input->mem);
-	tmp_memory = ft_strdup(input->ptr_nl + 1);
-	if (!tmp_memory)
-		return (free_memory(input));
-	*(input->ptr_nl + 1) = '\0';
-	ret_line = ft_strdup(input->mem);
-	if (!ret_line)
-		return (free_memory(input));
-	free(input->mem);
-	input->mem = tmp_memory;
-	input->ptr_nl = ft_strchr(input->mem, '\n');
-	if (!input->ptr_nl)
-		input->sts = CONTINUE;
-	return (ret_line);
-}
-
-void	read_buffer(int fd, char *buff, t_minfo *input)
-{	
-	ssize_t	buf_size;
-	char	*tmp_mem;
-
-	buf_size = read(fd, buff, BUFFER_SIZE);
-	if (buf_size == -1)
+	if (*save == NULL)
+		return (gt_free(save, buf));
+	s_len = gt_strlen(*save);
+	line = malloc(s_len + 1);
+	if (line == NULL)
+		return (gt_free(save, buf));
+	i = 0;
+	while (i < s_len + 1)
 	{
-		input->sts = ERROR;
-		return ;
+		line[i] = (*save)[i];
+		if ((*save)[i] == '\n')
+		{
+			line[i + 1] = '\0';
+			gt_strlcpy(save, i + 1, s_len - i - 1);
+			gt_free(NULL, buf);
+			return (line);
+		}
+		i++;
 	}
-	buff[buf_size] = '\0';
-	tmp_mem = ft_strjoin(input->mem, buff);
-	if (!tmp_mem)
-	{
-		input->sts = ERROR;
-		return ;
-	}
-	free(input->mem);
-	input->mem = tmp_mem;
-	input->ptr_nl = ft_strchr(input->mem, '\n');
-	if (input->ptr_nl)
-		input->sts = RETURN;
-	else if (buf_size == 0)
-		input->sts = END;
+	gt_free(save, buf);
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_minfo	input[FD_MAX];
-	char			*buff;
+	char		*buf;
+	static char	*save[FILE_OPEN_MAX];
+	int			bytes;
 
-	if (fd < 0 || FD_MAX <= fd || BUFFER_SIZE <= 0 || input[fd].sts == END)
+	if (BUFFER_SIZE < 1 || fd < 0 || FILE_OPEN_MAX < fd)
 		return (NULL);
-	if (!input[fd].mem)
+	buf = malloc((size_t)BUFFER_SIZE + 1);
+	if (buf == NULL)
+		return (gt_free(&save[fd], &buf));
+	while (1)
 	{
-		input[fd].mem = ft_strdup("");
-		if (!input[fd].mem)
+		if (gt_find_nl(save[fd]))
+			break ;
+		bytes = read(fd, buf, (size_t) BUFFER_SIZE);
+		if (bytes == -1)
+			return (gt_free(&save[fd], &buf));
+		buf[bytes] = 0;
+		if (bytes == 0)
+			break ;
+		if (gt_strjoin(&save[fd], &buf) == NULL)
 			return (NULL);
 	}
-	while (input[fd].sts == CONTINUE)
-	{
-		buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (!buff)
-			return (free_memory(&input[fd]));
-		read_buffer(fd, buff, &input[fd]);
-		free(buff);
-		buff = NULL;
-	}
-	if (input[fd].sts == ERROR)
-		return (free_memory(&input[fd]));
-	return (output_one_line(&input[fd]));
+	return (gt_strclip(&save[fd], &buf));
 }
