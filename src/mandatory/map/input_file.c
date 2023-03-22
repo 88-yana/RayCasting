6b3277c8a6277274073c7c6cf822859c5749ad98
88-yana <85784752+88-yana@users.noreply.github.com>
2023-03-23 07:15:32 +0900

    add:comment

--- src/mandatory/map/input_file.c
+++ src/mandatory/map/input_file.c
@@ -6,12 +6,18 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 20:50:17 by hyanagim          #+#    #+#             */
-/*   Updated: 2023/03/23 06:54:47 by hyanagim         ###   ########.fr       */
+/*   Updated: 2023/03/23 07:14:42 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+/**
+ * @brief ファイルの行数を計算する
+ * 
+ * @param fd 
+ * @return size_t 
+ */
 static size_t	calc_line_count(int fd)
 {
 	char	*temp;
@@ -32,6 +38,13 @@ static size_t	calc_line_count(int fd)
 	return (line_count);
 }
 
+/**
+ * @brief 一行ずつ取り，改行だったら，捨てる。でなければ，取得する
+ * 
+ * @param fd 
+ * @param line_count 
+ * @return char** 
+ */
 static char	**get_file_contents(int fd, size_t line_count)
 {
 	char	**file_contents;
@@ -54,6 +67,12 @@ static char	**get_file_contents(int fd, size_t line_count)
 	return (file_contents);
 }
 
+/**
+ * @brief ファイルの行数を取得する
+ * 
+ * @param filename 
+ * @return size_t 
+ */
 static size_t	get_line_count(const char *filename)
 {
 	int		fd;
@@ -65,6 +84,13 @@ static size_t	get_line_count(const char *filename)
 	return (line_count);
 }
 
+/**
+ * @brief ファイルの中身を取る
+ * 
+ * @param filename 
+ * @param line_count 
+ * @return char** 
+ */
 static char	**read_file(const char *filename, size_t line_count)
 {
 	int		fd;
@@ -76,6 +102,12 @@ static char	**read_file(const char *filename, size_t line_count)
 	return (file_contents);
 }
 
+/**
+ * @brief ファイルの行数を取得して，フィアルの中身を取得し，構造体に入れる
+ * 
+ * @param game 
+ * @param filename 
+ */
 void	input_file(t_game *game, const char *filename)
 {
 	size_t	line_count;
