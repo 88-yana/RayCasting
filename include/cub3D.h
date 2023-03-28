0cf71300ad08c68f45de36e605f9867afa5cf7e5
tmuramat <mt15hydrangea@gmail.com>
2023-03-29 00:50:06 +0900

    ADD: 型定義を別のヘッダーファイルに分けた

--- include/cub3D.h
+++ include/cub3D.h
@@ -8,85 +8,10 @@
 # include <errno.h>
 # include "get_next_line.h"
 # include "ft_printf.h"
-# include "libft.h"
 # include "mlx.h"
 # include "message.h"
 # include "config.h"
-
-typedef struct s_dictionary
-{
-	char	**key;
-	char	**value;
-}	t_dictionary;
-
-typedef struct s_rgb {
-	int	r;
-	int	g;
-	int	b;
-}	t_rgb;
-
-typedef struct s_path_to_texture {
-	char	*north;
-	char	*south;
-	char	*west;
-	char	*east;
-}	t_path_to_texture;
-
-typedef struct s_image {
-	void	*north;
-	void	*south;
-	void	*west;
-	void	*east;
-}	t_image;
-
-typedef struct s_map_info {
-	t_path_to_texture	path;
-	t_rgb				floor;
-	t_rgb				ceiling;
-	size_t				height;
-	size_t				width;
-	size_t				nswt[4];
-	size_t				n;
-	size_t				s;
-	size_t				w;
-	size_t				t;
-}	t_map_info;
-
-typedef struct s_player_info {
-	t_vec	pos;
-	t_vec	dir;
-	float	distance_to_wall;
-	float	wall_height;
-	bool	n_wall;
-	bool	s_wall;
-	bool	w_wall;
-	bool	e_wall;
-}	t_player_info;
-
-typedef struct s_game {
-	char			**map;
-	t_map_info		map_info;
-	t_image			images;
-	t_player_info	player;
-	int				key_code;
-	void			*mlx;
-	void			*win;
-}	t_game;
-
-typedef enum e_input_key {
-	MOVE_NONE = 0,
-	MOVE_FORWARD = 1 << 0,
-	MOVE_BACKWARD = 1 << 1,
-	MOVE_RIGHT = 1 << 2,
-	MOVE_LEFT = 1 << 3,
-	ROTATE_RIGHT = 1 << 4,
-	ROTATE_LEFT = 1 << 5,
-	GAME_EXIT = 1 << 6,
-	MOVE = MOVE_FORWARD | MOVE_BACKWARD | MOVE_RIGHT | MOVE_LEFT,
-	ROTATE = ROTATE_RIGHT | ROTATE_LEFT,
-	CONFIG = GAME_EXIT,
-	ALL = ~0,
-}	t_input_key;
+# include "types.h"
 
 //check
 void	check_arg(int argc, char **argv);
@@ -118,5 +43,4 @@ int		create_trgb(int t, int r, int g, int b);
 
 //debug
 void	print_debug_info(t_game *game);
-
 #endif
--- src/mandatory/init_game/init.c
+++ src/mandatory/init_game/init.c
@@ -1,5 +1,16 @@
 #include "cub3D.h"
 
+void	init_image(t_game *game)
+{
+	t_image	*img;
+
+	img = &game->img;
+	img->img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT);
+	img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->line_len, &img->endian);
+}
+
+
+
 /**
  * @brief MLXの初期化処理
  *
@@ -9,6 +20,7 @@ void	init_game(t_game *game)
 {
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
+	init_image(game);
 	load_images(game);
 	load_player(game);
 	set_event_hooks(game);
