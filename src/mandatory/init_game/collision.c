dd4c158d22d031dcf7ab48ce666fda799094a507
tmuramat <mt15hydrangea@gmail.com>
2023-04-07 02:03:13 +0900

    FIX: 微妙にリファクタリング

--- src/mandatory/init_game/collision.c
+++ src/mandatory/init_game/collision.c
@@ -4,12 +4,7 @@
 
 bool	is_collided_with_wall(t_vec p, char **map)
 {
-	int	x;
-	int	y;
-
-	x = floorf(p.x);
-	y = floorf(p.y);
-	if (map[y][x] == '1')
+	if (map[(int)p.y][(int)p.x] == '1')
 		return (true);
 	return (false);
 }
@@ -32,21 +27,21 @@ t_vec	get_current_direction(t_game *game)
 
 t_vec	get_next_position(t_vec pos, t_vec dir)
 {
-	t_vec	next;
+	t_vec	next_pos;
 
 	dir.y = -dir.y;
-	next = ft_add_vec(pos, ft_mul_vec(dir, COLLISION_MARGIN));
-	return (next);
+	next_pos = ft_add_vec(pos, ft_mul_vec(dir, COLLISION_MARGIN));
+	return (next_pos);
 }
 
 void	check_collision(t_game *game)
 {
-	t_vec	pos;
+	t_vec	next_pos;
 	t_vec	dir;
 
 	dir = get_current_direction(game);
-	pos = get_next_position(game->player.pos, dir);
-	if (is_collided_with_wall(pos, game->map))
+	next_pos = get_next_position(game->player.pos, dir);
+	if (is_collided_with_wall(next_pos, game->map))
 	{
 		game->key_store &= ~MOVE;
 	}
